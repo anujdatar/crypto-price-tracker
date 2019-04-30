8f96bd90c1141f58ce3219658a5918fab66343a5
Anuj Datar <anuj.datar@gmail.com>
2019-04-30 10:35:59 -0400

    added simple splashscreen and revert to basic app

--- main.js
+++ main.js
@@ -1,67 +1,38 @@
-// require('electron-reload')(__dirname)
 const electron = require('electron')
+
 const url = require('url')
 const path = require('path')
+
 const Store = require('electron-store')
-// const Toaster = require('electron-toaster')
-// const ipcMain = require('electron').ipcMain
-require('./src/menu')
 
 const{app, BrowserWindow, ipcMain} = electron
 
-let mainWindow
-// let toaster = new Toaster()
-
-function createWindow() {
-  // Create main browser window
+app.on('ready', () => {
+  // create main browser window
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    icon: path.join(__dirname, './assets/images/bitcoin.png'),
-    title: 'BitCoin Price Tracker'
-  })
+    titleBarStyle: 'hidden',
+    width: 1920,
+    height: 1080,
+    show: false // don't show the main window
+  });
 
-  // load index.html of the app in main window
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, './sections/index.html'),
-    protocol: 'file',
-    slashes: true
-  }))
+  // create a new `splash`-Window 
+  splash = new BrowserWindow({
+    width: 810,
+    height: 610,
+    // transparent: true,
+    frame: false,
+    alwaysOnTop: true
+  });
 
-  mainWindow.on('closed', () => {
-    // mainWindow = null
-    app.quit()
-  })
-
-  //Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-}
-
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-app.setAppUserModelId('com.ajax.cryptracker')
-
-// exports.close_all = function() {
-//   app.quit()
-// }
-
-ipcMain.on('update-notify-value', (event, arg) => {
-  mainWindow.webContents.send('targetPriceVal', arg)
-})
-
-// ipcMain.on('electron-toaster-message', (message) => {
-//   toaster.init(mainWindow)
-// })
+  splash.loadURL(`file://${__dirname}/src/html/splash.html`);
+  mainWindow.loadURL(`file://${__dirname}/src/html/index.html`);
+  
+  // if main window is ready to show, then destroy the splash window and show up the main window
+  mainWindow.once('ready-to-show', () => {
+    setTimeout(function() {
+      splash.destroy();
+      mainWindow.show();
+    }, 5000)
+  });
+});
---
+++ main1.js
@@ -0,0 +1,67 @@
+// require('electron-reload')(__dirname)
+const electron = require('electron')
+const url = require('url')
+const path = require('path')
+const Store = require('electron-store')
+// const Toaster = require('electron-toaster')
+// const ipcMain = require('electron').ipcMain
+require('./src/menu')
+
+const{app, BrowserWindow, ipcMain} = electron
+
+let mainWindow
+// let toaster = new Toaster()
+
+function createWindow() {
+  // Create main browser window
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, './assets/images/bitcoin.png'),
+    title: 'BitCoin Price Tracker'
+  })
+
+  // load index.html of the app in main window
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, './sections/index.html'),
+    protocol: 'file',
+    slashes: true
+  }))
+
+  mainWindow.on('closed', () => {
+    // mainWindow = null
+    app.quit()
+  })
+
+  //Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+app.setAppUserModelId('com.ajax.cryptracker')
+
+// exports.close_all = function() {
+//   app.quit()
+// }
+
+ipcMain.on('update-notify-value', (event, arg) => {
+  mainWindow.webContents.send('targetPriceVal', arg)
+})
+
+// ipcMain.on('electron-toaster-message', (message) => {
+//   toaster.init(mainWindow)
+// })
